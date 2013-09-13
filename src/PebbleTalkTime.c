cc8c391b313d0d9b79384f3c6f45194f68b8aa1d
Antoine <antoine@crafting-labs.fr>
2013-09-13 17:43:40 +0200

    Add app info
    - set version to 0.1
    - set name Talk Timer
    - and author (me :)

--- src/PebbleTalkTime.c
+++ src/PebbleTalkTime.c
@@ -5,8 +5,8 @@
 
 #define MY_UUID { 0xD3, 0x8B, 0x2F, 0x08, 0x04, 0xAB, 0x4A, 0x68, 0xB2, 0xF7, 0x1E, 0xB4, 0xD2, 0x64, 0x4C, 0xFE }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
-             1, 0, /* App version */
+             "Talk Timer", "Antoine Vernois // Crafting Labs",
+             0, 1, /* App version */
              DEFAULT_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
