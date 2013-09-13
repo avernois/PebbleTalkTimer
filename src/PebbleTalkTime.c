a0705fd0e4e8b0e165e3b9b419d6ad09ec7c9b46
Antoine <antoine@crafting-labs.fr>
2013-09-13 17:30:13 +0200

    empty pebble app init

---
+++ src/PebbleTalkTime.c
@@ -0,0 +1,28 @@
+#include "pebble_os.h"
+#include "pebble_app.h"
+#include "pebble_fonts.h"
+
+
+#define MY_UUID { 0xD3, 0x8B, 0x2F, 0x08, 0x04, 0xAB, 0x4A, 0x68, 0xB2, 0xF7, 0x1E, 0xB4, 0xD2, 0x64, 0x4C, 0xFE }
+PBL_APP_INFO(MY_UUID,
+             "Template App", "Your Company",
+             1, 0, /* App version */
+             DEFAULT_MENU_ICON,
+             APP_INFO_STANDARD_APP);
+
+Window window;
+
+
+void handle_init(AppContextRef ctx) {
+
+  window_init(&window, "Window Name");
+  window_stack_push(&window, true /* Animated */);
+}
+
+
+void pbl_main(void *params) {
+  PebbleAppHandlers handlers = {
+    .init_handler = &handle_init
+  };
+  app_event_loop(params, &handlers);
+}
